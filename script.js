7ea4d74f685b70bc926e01002a711abcf4ae3129
eimdadul <eimdadulhaque@gmail.com>
2022-01-07 13:21:53 +0600

    create task

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 
+var comUl = document.querySelector("#com-ul");
+var inconUl = document.querySelector("#incom-ul");
+var todoForm = document.querySelector("#todo-form");
+
+var createTask = function (task) {
+    let todoList = document.createElement("li");
+    let todoLable = document.createElement("label");
+    let todoInput = document.createElement("input");
+
+    todoLable.innerText = task;
+    todoInput.type ="chackbox";
+    todoList.appendChild(todoLable);
+    todoList.appendChild(todoInput);
+
+    return todoList;
+}
+
+
+var addTask = function () {
+    
+}
